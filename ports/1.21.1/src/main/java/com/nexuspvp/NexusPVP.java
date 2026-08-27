ff6aa766aad585a52c012b554a13aafc111eab7d
Angina830 <zakurnaevgleb666@gmail.com>
2026-08-27 09:13:11 +0300

    fix: registered ClientTickEvents.END_CLIENT_TICK in NexusPVP client initializer enabling DamageIndicator ticks and combos

--- ports/1.21.1/src/main/java/com/nexuspvp/NexusPVP.java
+++ ports/1.21.1/src/main/java/com/nexuspvp/NexusPVP.java
@@ -4,6 +4,7 @@ import com.nexuspvp.config.ConfigManager;
 import com.nexuspvp.gui.ThemeManager;
 import com.nexuspvp.module.ModuleManager;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 
 public class NexusPVP implements ClientModInitializer {
@@ -20,6 +21,12 @@ public class NexusPVP implements ClientModInitializer {
         configManager = new ConfigManager();
 
         configManager.loadConfig();
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (moduleManager != null) {
+                moduleManager.onTick();
+            }
+        });
 
         WorldRenderEvents.LAST.register(context -> {
             if (moduleManager != null && context.matrixStack() != null) {
