4f3886b3d59b5046c54565cbc363527cfbaa6a53
Angina830 <zakurnaevgleb666@gmail.com>
2026-08-27 09:57:50 +0300

    refactor: cleaned up all temporary test files, dumps and debris from repository

--- test2.java
+++
@@ -1,9 +0,0 @@
-package test;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
-public class Test {
-    public void test() {
-        WorldRenderEvents.LAST.register(context -> {
-            
-        });
-    }
-}
